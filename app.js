7ffa43b5f36b5544ddf12869e70c268f60d4dc9a
thevon1 <evon@kogosoftwarellc.com>
2017-06-07 11:00:34 -0700

    added files

---
+++ app.js
@@ -0,0 +1,85 @@
+var myApp = angular.module('myApp', ['ui.router', 'ui.bootstrap']);
+        
+        myApp.config(function($locationProvider, $stateProvider) {
+        $locationProvider.html5Mode(true)
+        
+var profileState = {
+    name: 'profile',
+    url: '/profile',
+    templateUrl: 'views/profile.html'
+    }
+    $stateProvider.state(profileState);
+  
+var editEntryState = {
+    name: 'editEntry',
+    url: '/editEntry',
+    templateUrl: 'views/editEntry.html'
+    }
+    $stateProvider.state(editEntryState);
+            
+var entryListState = {
+    name: 'entryList',
+    url: '/entryList',
+    templateUrl: 'views/entryList.html'
+    }
+    $stateProvider.state(entryListState);        
+    
+var homeState = {
+    name: 'home',
+    url: '/home',
+    templateUrl: 'views/home.html'
+    }
+    $stateProvider.state(homeState);            
+    
+var labelsState = {
+    name: 'labels',
+    url: '/labels',
+    templateUrl: 'views/labels.html'
+    }
+    $stateProvider.state(labelsState);
+            
+    });
+    
+    
+    // button controller 
+
+myApp.controller('buttonCtrl', function ($scope, $uibModal, $attrs, timeService) {
+
+        
+        console.dir($attrs);
+
+        $scope.openModal = function(){
+            $uibModal.open({
+                templateUrl:'modalContent.html',      //modalContent.html file to spice up the modal
+                backdrop: $attrs.backdrop === "true"})
+        }
+        $scope.openSecondModal = function(){
+            $uibModal.open({
+                templateUrl:'modalContent.html',      //modalContent.html file to spice up the modal
+                backdrop: $attrs.backdrop === "true",
+                size: "sm"})
+        }
+        $scope.setMyMessage = function(){
+            $scope.myMessage = timeService.currentTime()
+        }
+        $scope.modalButton = function(){
+            var uibModalInstance = $uibModal.open();
+        }
+        
+    }
+    
+);
+    
+myApp.service("timeService", function Time(){
+    this.currentTime = function(){
+        return (new Date()).toString()
+    }
+})
+    
+//myApp.factory("myMessage", function(){
+//    return "Hello!";
+//})
+// add my message as dependancy on .controller to use
+
+    
+
---
+++ calculator.js
@@ -0,0 +1,73 @@
+var calcApp = angular.module('calcApp', []);
+
+calcApp.controller('calcCtrl', function ($scope) {
+    $scope.leftOperand = null;
+    $scope.operator = null;
+    $scope.rightOperand = null;
+    $scope.answer = null;
+
+
+    $scope.setOperand = function (operandEntered) {
+
+        if ($scope.leftOperand === null) {
+            $scope.leftOperand = operandEntered;
+        } else if ($scope.operator === null) {
+            $scope.leftOperand = $scope.leftOperand + operandEntered;
+        } else if ($scope.rightOperand === null) {
+            $scope.rightOperand = operandEntered;
+        } else if ($scope.answer === null) {
+            $scope.rightOperand = $scope.rightOperand + operandEntered;
+        }
+    };
+
+    $scope.setOperator = function (operatorEntered) {
+        $scope.operator = operatorEntered;
+    };
+
+    $scope.getAnswer = function () {
+        var result = $scope.leftOperand + $scope.operator + $scope.rightOperand;
+        var answer = eval(result);
+        if (answer % 2 !== 0){
+            $scope.answer = answer.toFixed(3);
+        }
+        else {$scope.answer = answer;}
+    };
+
+    $scope.setClear = function (a) {
+        $scope.clear = location.reload();
+    };
+
+});
+
+    var windowProperties = "width=300,height=450,resizable,scrollbars=yes";
+    var windowObjectReference = function openCalc() {
+        window.open("http://127.0.0.1:8080/calculatorHTML.html", "calcWindow", windowProperties);
+};
+
+
+
+
+
+//calcApp.controller('buttonCtrl', function ($scope, $uibModal, $attrs) {
+//
+//        
+//        console.dir($attrs);
+//
+//        $scope.openModal = function(){
+//            $uibModal.open({
+//                templateUrl:'calculatorTemplate.html',      //modalContent.html file to spice up the modal
+//                backdrop: $attrs.backdrop === "true"});
+//        };
+//        $scope.openSecondModal = function(){
+//            $uibModal.open({
+//                templateUrl:'modalContent.html',      //modalContent.html file to spice up the modal
+//                backdrop: $attrs.backdrop === "true",
+//                size: "sm"});
+//        };
+//        $scope.modalButton = function(){
+//            var uibModalInstance = $uibModal.open();
+//        };
+//        
+//    }
+//    
+//);
