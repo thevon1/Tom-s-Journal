3818b481c6835f78a462dd65615d10d79cdc8d82
thevon1 <evon@kogosoftwarellc.com>
2017-06-08 13:42:59 -0700

    condensed the if/else if  code in the setOperand() function

--- calculator.js
+++ calculator.js
@@ -1,27 +1,22 @@
 var calcApp = angular.module('calcApp', []);
 
 calcApp.controller('calcCtrl', function ($scope) {
-    $scope.leftOperand = null;
-    $scope.operator = null;
-    $scope.rightOperand = null;
-    $scope.answer = null;
+    $scope.leftOperand = "";
+    $scope.operator = "";
+    $scope.rightOperand = "";
+    $scope.answer = "";
 
 
-    $scope.setOperand = function (operandEntered) {
-
-        if ($scope.leftOperand === null) {
-            $scope.leftOperand = operandEntered;
-        } else if ($scope.operator === null) {
-            $scope.leftOperand = $scope.leftOperand + operandEntered;
-        } else if ($scope.rightOperand === null) {
-            $scope.rightOperand = operandEntered;
-        } else if ($scope.answer === null) {
-            $scope.rightOperand = $scope.rightOperand + operandEntered;
-        }
+        $scope.setOperand = function (operandEntered) {
+            if ($scope.leftOperand === "" && $scope.operator === ""){
+                $scope.leftOperand = operandEntered;
+            }
+            else {$scope.rightOperand = operandEntered;}
     };
 
     $scope.setOperator = function (operatorEntered) {
         $scope.operator = operatorEntered;
+        console.log($scope.operator);
     };
 
     $scope.getAnswer = function () {
